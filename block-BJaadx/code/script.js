adcddceeda51e6c5174b2f0442682f57892f4923
chirag gaur <chiragwalker@gmail.com>
2023-04-25 22:24:13 +0530

    all done

--- block-BJaadx/code/script.js
+++ block-BJaadx/code/script.js
@@ -10,18 +10,19 @@
    //  document.children[0].children[1].children[5].children[0].innerHTML = "all about document";
 
 //    - Select the input element with name `email`
-   // document.children[0].children[1].children[6].children[2].name;
+   // document.bosy.children[6].children[2].name;
 
 // 3. Log the number (using console.log) of children of all the `li` element inside the ul with class `topics`
-      // doubt
-      //console.log(document.children[0].children[1].children[5].length());
 
+    //   let ul = document.body.children[5];
+    //   console.log(`The length of li elements is ${ul.children.length}`);
 // 4. Select the first input using the `type` selector and store them in variable named `emailInput`
      // doubt
-     //let emailInput = document.chlidren[0].children[1].children[6].children[2].type;
-
+    //  let emailInput = document.querySelector('input');
+    //   console.log(emailInput);
 // 5. Select the ul element using class selector and store in `topics`
-      let topics = document.querySelectorAll('.list');
+      let topics = document.querySelector('.topics');
+    //   console.log(topics);
 
 // 6. Select the first label element and store in `label`
      // let label = document.querySelector('label');
@@ -33,7 +34,7 @@
     // let password = document.querySelector(input[type="password"]);
 // 9. Select the input using the placeholder attribute selector with value `password` and store in `attrPassword`
     // doubt
-//    password = document.querySelector('input[type="password"]');
+//   let attribute = document.querySelector('input[placeholder="password"]');
 
 // 10. Select all the `li` element and store in `allTopics`
     // document.querySelectorAll('li');
@@ -49,7 +50,7 @@
 
 // 14. Select the first li element inside the `ul` element using `>` (direct child) and store in `firstLi`
         // doubt
-        // let firstLi = document.children[0].children[1].children[5]>'li';
+        // let firstLi = document.querySelector('ul>li'); 
 
 // 15. Select all the img element and log the number of element saying `The total number of img element is ---`
     //   let element = document.querySelectorAll('img').length;
@@ -71,16 +72,19 @@
 
 // 21. Select the parent element of the element stored in `topics` variable (#5) and log the element.
       //doubt
-    //   console.log(`${topics}`.nextElementSibling);
+    //   console.log(topics.parentElement);
 
 // 22. Select the next element sibling of the element stored in `topics` variable (#5) and log the element.
-    // doubt
+    // console.log(topics.nextElementSibling);
+
 // 23. Select the previous element sibling of the element stored in `topics` variable (#5) and change the `innerText` property to `Learning About Walking the DOM`.
-      //doubt
+    //   console.log(topics.previousElementSibling);
+
 // 24. Select the first element child of the element stored in `topics` variable (#5) and change the `innerText` property of the element to `This is the first child element`.
-      //doubt
+    //   console.log(topics.firstElementChild);
 // 25. Select the last element child of the element stored in `topics` variable (#5) and log the `typeof` the element.
-      //doubt
+    //   console.log(topics.lastElementChild);
 // 26. Select the element with type `fieldset` and store in a variable named `fieldsetElm`.
-         let fieldsetElm = document.querySelector('input[type="fieldset"');
+        let fieldsetElm = document.querySelector('fieldset');
 // 27. Select the parent element of the element stored in `fieldsetElm` variable (#5) and log the `typeof` the element.
+       console.log(typeof fieldsetElm.parentElement) ;
